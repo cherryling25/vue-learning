0946b1d804fc51ee3c9ab0e9f376607db3449265
Cherry <cherryling25@outlook.com>
2019-04-16 22:18:32 +0800

    修改

--- 字符串反转.js
+++ 字符串反转.js
@@ -2,6 +2,11 @@ var one = new Vue({
     el:"#vue-app",
     data:{
         message: 'Runoob!'
-    }
+    },
+    methods: {
+        reverseMessage: function () {
+          this.message = this.message.split('').reverse().join('')
+        }
+      }
    
 });
