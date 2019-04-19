2921049b361c5763bed7b26f8b3b1c8afd893dbc
Cherry <cherryling25@outlook.com>
2019-04-19 11:12:16 +0800

    增加

---
+++ 过滤器.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:"#vue-app",
+    data: {
+        message: 'runoob'
+      },
+      filters: {
+        capitalize: function (value) {
+          if (!value) return ''
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+      }
+   
+});
